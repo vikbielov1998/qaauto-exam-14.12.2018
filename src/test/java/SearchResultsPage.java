24f7d5d0078c2d55acd7008dfd83ed3d39b0eaf3
Viktor Bielov <vikbelov@ukr.net>
2018-12-14 21:29:22 +0200

    Final push

--- src/test/java/SearchResultsPage.java
+++ src/test/java/SearchResultsPage.java
@@ -3,52 +3,105 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-public class SearchResultsPage {
+import static java.lang.Thread.sleep;
 
-    public WebDriver webDriver;
+class SearchResultsPage {
 
-    @FindBy (xpath = "//input[@name='q']")
+    private WebDriver webDriver;
+
+    @FindBy(xpath = "//input[@name='q']")
     private WebElement searchField;
 
-    @FindBy (xpath = "//div[@class='srg']/div[@class='g']")
-    private List<WebElement> resultsList;
+    @FindBy(xpath = "//div[@class='srg']/div[@class='g']")
+    private List<WebElement> resultsList1;
 
-    @FindBy (xpath = "//a[@aria-label='Page 2']")
+    @FindBy(xpath = "//div[@class='srg']/div[@class='g']")
+    private List<WebElement> resultsList2;
+
+    @FindBy(xpath = "//a[@aria-label='Page 2']")
     private WebElement linkToNextTab;
 
-    public SearchResultsPage(WebDriver webDriver) {
+    /**
+     * Constructor of SearchResultsPage class;
+     *
+     * @param webDriver - webDriver instance from SearchTest class.
+     */
+    SearchResultsPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
     }
 
-    public void search(String searchTerm) {
+    /**
+     * Method to enter search term to field and press "Enter" button;
+     *
+     * @param searchTerm - term we want to find.
+     */
+    void search(String searchTerm) {
+        WebDriverWait wait = new WebDriverWait(webDriver, 5);
+        wait.until(ExpectedConditions.visibilityOf(searchField));
         searchField.sendKeys(searchTerm);
         searchField.sendKeys(Keys.ENTER);
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int getSizeOfList(){
-        return resultsList.size();
+    /**
+     * Method to get size of result list of necessary elements on first page;
+     *
+     * @return returns size of result list.
+     */
+    int getSizeOfList1() {
+        return resultsList1.size();
     }
 
-    public boolean searchOnTabOne(String searchTerm){
+    /**
+     * Method to check if all result element on first page contains search term;
+     *
+     * @param searchTerm - term we want to find;
+     * @return returns false if at least one result doesn't contain search term, otherwise returns true.
+     */
+    boolean searchOnTabOne(String searchTerm) {
 
-        for (WebElement result : resultsList){
+        for (WebElement result : resultsList1) {
             if (!result.getText().toLowerCase().contains(searchTerm.toLowerCase())) return false;
         }
 
         return true;
     }
 
-    public void goToNextTab(){
+    /**
+     * Method to switch to page 2.
+     */
+    void goToNextTab() {
         linkToNextTab.click();
     }
 
-    public boolean searchOnTabTwo(String searchTerm){
+    /**
+     * Method to get size of result list of necessary elements on second page;
+     *
+     * @return returns size of result list.
+     */
+    int getSizeOfList2() {
+        return resultsList2.size();
+    }
 
-        for (WebElement result : resultsList){
+    /**
+     * Method to check if all result element on second page contains search term;
+     *
+     * @param searchTerm - term we want to find;
+     * @return returns false if at least one result doesn't contain search term, otherwise returns true.
+     */
+    boolean searchOnTabTwo(String searchTerm) {
+
+        for (WebElement result : resultsList2) {
             if (!result.getText().toLowerCase().contains(searchTerm.toLowerCase())) return false;
         }
 
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,44 +1,66 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class SearchTest {
 
-    public WebDriver webDriver;
-
-    private SearchResultsPage searchResultsPage;
+    private WebDriver webDriver;
 
 
+    /**
+     * Method runs at the begining of all tests. Sets up browser we want to use and goes to Google.com;
+     *
+     * @param browserName - browser we want to use.
+     */
+    @Parameters("browserName")
     @BeforeMethod
-    public void beforeMethod() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new FirefoxDriver();
+    public void beforeMethod(@Optional("chrome") String browserName) {
+        if (browserName.toLowerCase().equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        }
+
+        if (browserName.toLowerCase().equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else {
+            try {
+                throw new Exception("browserName " + browserName + " is not supported.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         webDriver.get("https://www.google.com");
     }
 
+    /**
+     * Test to check if size of result list equals to 10 and all results contains search term on first two pages.
+     */
     @Test
     public void searchTest() {
         String searchTerm = "Selenium";
 
-        searchResultsPage = new SearchResultsPage(webDriver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(webDriver);
 
         searchResultsPage.search(searchTerm);
-        Assert.assertEquals(searchResultsPage.getSizeOfList(), 10, "Size of result list on first page is not equal to 10");
+        Assert.assertEquals(searchResultsPage.getSizeOfList1(), 10, "Size of result list on first page is not equal to 10");
 
         Assert.assertTrue(searchResultsPage.searchOnTabOne(searchTerm), "Not all results on first page contains searchTerm");
 
         searchResultsPage.goToNextTab();
-        Assert.assertEquals(searchResultsPage.getSizeOfList(), 10, "Size of result list on second page is not equal to 10");
+        Assert.assertEquals(searchResultsPage.getSizeOfList2(), 10, "Size of result list on second page is not equal to 10");
 
         Assert.assertTrue(searchResultsPage.searchOnTabTwo(searchTerm), "Not all results on second page contains searchTerm");
     }
 
+    /**
+     * Method runs at end of all tests. Closes the browser.
+     */
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         webDriver.quit();
     }
 }
